2843a867dc9ab020ad46da886d549c07ae50ba68
Yash Raj <89970907+yash-raj202134@users.noreply.github.com>
2024-04-30 16:41:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
-from src.logger import logging
 
-from src.exception import CustomException
-
-import sys
-
-
-# if __name__=='__main__':
-#     try:
-#         a = 1/0
-#     except Exception as e:
-#         logging.info("Divided by 0")
-#         raise CustomException(e,sys)
-
-
-import sys
-
-# Print out the list of directories in sys.path
-print(sys.path)
